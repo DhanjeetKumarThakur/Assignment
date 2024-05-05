f0cb09917739c93421b070237c604455d4cda8e1
Dhanjeet Kumar Thakur <68640018+DhanjeetKumarThakur@users.noreply.github.com>
2024-05-05 20:34:41 +0530

    uploading the files

---
+++ src/main/java/com/learning/securityConfig/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.learning.securityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public UserDetailsService getUserDetailsService() {
+		return new UserDetailsServiceImpl();
+	}
+
+	// We need the bean of BCryptPasswordEncoder for encoding our password
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// We need bean of DaoAuthenticationProvider because we will be our providing
+	// security for dao(memory/db) objects
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsService());
+		daoAuthenticationProvider.setPasswordEncoder(this.passwordEncoder());
+		return daoAuthenticationProvider;
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {	 			
+		return http.csrf(obj->obj.disable())
+                .authorizeHttpRequests(auth -> {
+                            auth.requestMatchers("/home/admin/**").hasRole("ADMIN");
+                            auth.requestMatchers("/home/user/*").hasRole("NORMAL_USER");
+                            auth.requestMatchers("/**").permitAll();
+                            
+                            auth.anyRequest().authenticated();
+                        }
+                )
+                .formLogin(obj->obj
+                		.loginPage("/login")
+                		.loginProcessingUrl("/dologin")
+                		.defaultSuccessUrl("/home/admin/index")
+                		)
+                .build();
+	}
+}
